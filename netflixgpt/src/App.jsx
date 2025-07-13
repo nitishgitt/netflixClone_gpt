9edd7398e69929839b04bfb55ccdd955daed890e
nitishgitt <nitish.gitt@gmail.com>
2025-07-14 01:38:32 +0530

    testing the git push

--- netflixgpt/src/App.jsx
+++ netflixgpt/src/App.jsx
@@ -1,7 +1,10 @@
-
+import Home from "./components/homepage"
 function App() {
   return (
+    <>
 <h1 className='text-red-700 text-8xl px-3.5 py-2.5 m-auto text-center bg-amber-300 font-extrabold rounded-4xl'>hello nitish is test</h1>
+<Home/>
+    </>
   )
 }
 
---
+++ netflixgpt/src/components/homepage.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return(
+        <>
+        <h1 className="bg-red-600 text-yellow-500 font-extrabold px-2.5  text-8xl py-3.5 m-auto rounded-3xl text-center">this is home nitish</h1>
+        </>
+    )
+}
+export default Home;
