6528da98418e92df3a90f21d0b6e36e23a7376fc
nitishgitt <nitish.gitt@gmail.com>
2025-07-14 01:29:57 +0530

    initial commit changes

---
+++ netflixgpt/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+<h1 className='text-red-700 text-8xl px-3.5 py-2.5 m-auto text-center bg-amber-300 font-extrabold rounded-4xl'>hello nitish is test</h1>
+  )
+}
+
+export default App
